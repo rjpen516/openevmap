e737bc6b02706432ad621a1a305f8aafdfc6666e
Richard Penshorn <richard@penshorn.net>
2017-02-04 17:40:50 -0600

    finished tutorials

--- server/openevmap_old/data_collection/models.py
+++ server/openevmap_old/data_collection/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.contrib.auth.models import User
+import settings
 
 # Create your models here.
 class EVPoint(models.Model):
@@ -10,7 +10,7 @@ class EVPoint(models.Model):
     tempature =  models.IntegerField()
     speed = models.DecimalField(max_digits=5,decimal_places=2)
     energy_usage = models.DecimalField(max_digits=10,decimal_places=3)
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
 
     def __str__(self):
         return ("%d:%d @ %d"%(self.longitude,self.latitude, self.speed))
