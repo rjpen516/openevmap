7b0d3d8a2166c077d35c3720c99cb13a5c32417e
Richard Penshorn <richard@penshorn.net>
2017-02-04 14:32:22 -0600

    working with clases

--- server/openevmaps/openevmaps/api/models.py
+++ server/openevmaps/openevmaps/api/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 
+
 # Create your models here.
 class EVPoint(models.Model):
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
@@ -11,7 +12,8 @@ class EVPoint(models.Model):
     tempature =  models.IntegerField()
     speed = models.DecimalField(max_digits=5,decimal_places=2)
     energy_usage = models.DecimalField(max_digits=10,decimal_places=3)
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    #user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='evpoint', on_delete=models.CASCADE)
 
     def __str__(self):
         return ("%d:%d @ %d"%(self.longitude,self.latitude, self.speed))
