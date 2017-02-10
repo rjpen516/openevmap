b4278f558f4fd54e00ffdf1f25805a179680af2b
Richard Penshorn <richard@penshorn.net>
2017-02-09 21:25:54 -0600

    python3

--- server/openevmaps/openevmaps/api/admin.py
+++ server/openevmaps/openevmaps/api/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from models import EVPoint
+from api.models import EVPoint
 # Register your models here.
 admin.site.register(EVPoint)
